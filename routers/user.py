15f449d841f6feb6643797455c50d2abb8de14c7
atapasco <atapasco@unicesar.edu.co>
2024-09-21 21:07:50 -0500

    correccion de modulo user y creacion de service para user

--- routers/user.py
+++ routers/user.py
@@ -1,30 +1,24 @@
-from fastapi import APIRouter
-from config.bd import conn
-from models.user import users
-from schemas.user import User, UserCount
+# routes/user.py
+from fastapi import APIRouter, HTTPException
+from schemas.user import User
 from typing import List
-from starlette.status import HTTP_204_NO_CONTENT
-from sqlalchemy import func, select
-
-from cryptography.fernet import Fernet
+from service.user_service import create_user, get_all_users, authenticate_user
 
 user = APIRouter()
-key = Fernet.generate_key()
-f = Fernet(key)
 
-
-@user.get(
-    "/users",
-    tags=["users"],
-    response_model=List[User],
-    description="Get a list of all users",
-)
+@user.get("/users", tags=["users"], response_model=List[User], description="Get a list of all users")
 def get_users():
-    return conn.execute(users.select()).fetchall()
+    return get_all_users()
 
 @user.post("/user_post", tags=["users"], response_model=User, description="Create a new user")
-def create_user(user: User):
-    new_user = {"email": user.email, "role":user.role}
-    new_user["password"] = f.encrypt(user.password.encode("utf-8"))
-    result = conn.execute(users.insert().values(new_user))
-    return conn.execute(users.select().where(users.c.email == result.lastrowid)).first()
+def create_user_endpoint(user: User):
+    return create_user(user)
+
+# Nuevo endpoint para iniciar sesión
+@user.post("/login", tags=["users"], description="Authenticate a user")
+def login(email: str, password: str):
+    authenticated_user = authenticate_user(email, password)
+    if authenticated_user:
+        return {"message": "Login successful", "user": authenticated_user}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
